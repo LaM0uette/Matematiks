e73479fd6741a55fce7f56a3597488384267de1c
LaM0uette <LaM0uette.dev@gmail.com>
2024-01-15 18:52:28 +0100

    add ball spawner

--- Assets/Game/Modules/Board/Balls/BallSpawner.cs
+++ Assets/Game/Modules/Board/Balls/BallSpawner.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using Game.Modules.Board.Cells;
 using UnityEngine;
 
-public class BallSpawner : MonoBehaviour
+namespace Game.Modules.Board.Balls
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BallSpawner : MonoBehaviour
     {
-        
-    }
+        #region Statements
 
-    // Update is called once per frame
-    void Update()
-    {
+        [SerializeField] private GameObject _ballPrefab;
+        [SerializeField] private GameObject _firstCell;
+
+        #endregion
+
+        #region Events
+
+        private void LateUpdate()
+        {
+            if (!FirstCellIsEmpty())
+                return;
+            
+            SpawnBall();
+        }
+
+        #endregion
+
+        #region Functions
+
+        private bool FirstCellIsEmpty()
+        {
+            var firstCell = _firstCell.GetComponent<Cell>();
+            return firstCell.IsEmpty;
+        }
         
+        private void SpawnBall()
+        {
+            Instantiate(_ballPrefab, Vector3.zero, Quaternion.identity, _firstCell.transform);
+        }
+
+        #endregion
     }
 }
