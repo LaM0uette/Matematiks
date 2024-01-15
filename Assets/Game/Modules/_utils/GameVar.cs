5ff79fcd5d1b3e0b3ac77e7de7da6e13a6df5de6
LaM0uette <LaM0uette.dev@gmail.com>
2024-01-15 18:40:40 +0100

    add gameVar

---
+++ Assets/Game/Modules/_utils/GameVar.cs
@@ -0,0 +1,8 @@
+namespace Game.Modules._utils
+{
+    public static class GameVar
+    {
+        public static float BallDropDuration { get; private set; } = 0.2f;
+        public static float BallMoveDuration { get; private set; } = 0.1f;
+    }
+}
