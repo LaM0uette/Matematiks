2bf683f1f5bfa269f11cb90a65248e660feefc8d
LaM0uette <LaM0uette.dev@gmail.com>
2024-01-17 14:23:08 +0100

    fix

--- Assets/Game/Modules/Board/Balls/Ball.cs
+++ Assets/Game/Modules/Board/Balls/Ball.cs
@@ -61,7 +61,7 @@ namespace Game.Modules.Board.Balls
             var ballNumbers = GameManager.Instance.BallNumbers;
             var totalWeight = ballNumbers.Where(ballNumber => !ballNumber.IsLocked).Sum(ballNumber => ballNumber.Weight);
 
-            var randomNumber = Random.Range(1, totalWeight + 1);
+            var randomNumber = Random.Range(0, totalWeight + 1);
             var sum = 0f;
             
             foreach (var ballNumber in ballNumbers)
