6eaf1f363f7d1e43445e47ae48c93f4bcfbefe73
LaM0uette <LaM0uette.dev@gmail.com>
2024-01-24 08:10:55 +0100

    change colors

--- Assets/Game/Modules/Utils/ColorVar.cs
+++ Assets/Game/Modules/Utils/ColorVar.cs
@@ -8,13 +8,13 @@ namespace Game.Modules.Utils
         public static Color32 BlackColor = new(28, 28, 28, 255);
         
         public static Color32 Ball1Color = WhiteColor;
-        public static Color32 Ball2Color = new(152, 213, 235, 255);
-        public static Color32 Ball3Color = new(179, 238, 179, 255);
+        public static Color32 Ball2Color = new(133, 211, 227, 255);
+        public static Color32 Ball3Color = new(146, 222, 160, 255);
         public static Color32 Ball4Color = new(255, 247, 146, 255);
         public static Color32 Ball5Color = new(255, 197, 128, 255);
         public static Color32 Ball6Color = new(192, 119, 152, 255);
         public static Color32 Ball7Color = new(152, 140, 203, 255);
-        public static Color32 Ball8Color = new(56, 83, 131, 255);
-        public static Color32 OtherBallColor = new(102, 36, 45, 255);
+        public static Color32 Ball8Color = new(48, 96, 150, 255);
+        public static Color32 OtherBallColor = new(94, 23, 39, 255);
     }
 }
