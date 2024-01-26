6311b5f3ecfbf9a10e4de13bbe9a6324c89872fa
LaM0uette <LaM0uette.dev@gmail.com>
2024-01-26 08:49:10 +0100

    modify big balls value colors

--- Assets/Game/Modules/Board/Balls/Ball.cs
+++ Assets/Game/Modules/Board/Balls/Ball.cs
@@ -56,6 +56,9 @@ namespace Game.Modules.Board.Balls
 
         public void SetNum(int number)
         {
+            if (number > 99) 
+                return;
+            
             if (number < 1) number = 1;
             
             Number = number;
@@ -72,7 +75,7 @@ namespace Game.Modules.Board.Balls
             _tmpNumber.text = number.ToString();
         }
         
-        private static Color GetBallColor(int number) 
+        private static Color GetBallColor(int number)
         {
             return number switch
             {
@@ -84,7 +87,18 @@ namespace Game.Modules.Board.Balls
                 6 => ColorVar.Ball6Color,
                 7 => ColorVar.Ball7Color,
                 8 => ColorVar.Ball8Color,
-                _ => ColorVar.OtherBallColor
+                9 => ColorVar.Ball9Color,
+                10 or 20 or 30 or 40 or 50 or 60 or 70 or 80 or 90 => ColorVar.Ball10Color,
+                11 or 21 or 31 or 41 or 51 or 61 or 71 or 81 or 91 => ColorVar.Ball1ColorDark,
+                12 or 22 or 32 or 42 or 52 or 62 or 72 or 82 or 92 => ColorVar.Ball2ColorDark,
+                13 or 23 or 33 or 43 or 53 or 63 or 73 or 83 or 93 => ColorVar.Ball3ColorDark,
+                14 or 24 or 34 or 44 or 54 or 64 or 74 or 84 or 94 => ColorVar.Ball4ColorDark,
+                15 or 25 or 35 or 45 or 55 or 65 or 75 or 85 or 95 => ColorVar.Ball5ColorDark,
+                16 or 26 or 36 or 46 or 56 or 66 or 76 or 86 or 96 => ColorVar.Ball6ColorDark,
+                17 or 27 or 37 or 47 or 57 or 67 or 77 or 87 or 97 => ColorVar.Ball7ColorDark,
+                18 or 28 or 38 or 48 or 58 or 68 or 78 or 88 or 98 => ColorVar.Ball8ColorDark,
+                19 or 29 or 39 or 49 or 59 or 69 or 79 or 89 or 99 => ColorVar.Ball9ColorDark,
+                _ => ColorVar.Ball10ColorDark
             };
         }
         
--- Assets/Game/Modules/Utils/ColorVar.cs
+++ Assets/Game/Modules/Utils/ColorVar.cs
@@ -15,6 +15,18 @@ namespace Game.Modules.Utils
         public static Color32 Ball6Color = new(200, 135, 170, 255);
         public static Color32 Ball7Color = new(174, 164, 203, 255);
         public static Color32 Ball8Color = new(71, 120, 159, 255);
-        public static Color32 OtherBallColor = new(94, 32, 44, 255);
+        public static Color32 Ball9Color = new(92, 61, 82, 255);
+        public static Color32 Ball10Color = new(94, 32, 44, 255);
+        
+        public static Color32 Ball1ColorDark = new(200, 195, 202, 255);
+        public static Color32 Ball2ColorDark = new(113, 137, 165, 255);
+        public static Color32 Ball3ColorDark = new(94, 141, 79, 255);
+        public static Color32 Ball4ColorDark = new(228, 208, 132, 255);
+        public static Color32 Ball5ColorDark = new(155, 139, 89, 255);
+        public static Color32 Ball6ColorDark = new(123, 77, 100, 255);
+        public static Color32 Ball7ColorDark = new(108, 102, 132, 255);
+        public static Color32 Ball8ColorDark = new(51, 87, 114, 255);
+        public static Color32 Ball9ColorDark = new(64, 37, 54, 255);
+        public static Color32 Ball10ColorDark = new(59, 28, 29, 255);
     }
 }
