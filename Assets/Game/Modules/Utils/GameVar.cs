ac01beefb4df7d5176bbc1fc3ddd876ffaf5a26c
LaM0uette <LaM0uette.dev@gmail.com>
2024-02-01 14:48:45 +0100

    clean gamevar

--- Assets/Game/Modules/Utils/GameVar.cs
+++ Assets/Game/Modules/Utils/GameVar.cs
@@ -2,13 +2,25 @@ namespace Game.Modules.Utils
 {
     public static class GameVar
     {
+        #region SceneName
+
         public static string GameScene { get; private set; } = "Game";
         public static string MenuScene { get; private set; } = "Menu";
-        
+
+        #endregion
+
+        #region BallProperties
+
         public static float BallDropDuration { get; private set; } = 0.2f;
         public static float BallMoveDuration { get; private set; } = 0.1f;
-        
+
+        #endregion
+
+        #region BallWeightProperties
+
         public static float DefaultNewBallWeight { get; private set; } = 1f;
         public static float DefaultBallWeightDiviser { get; private set; } = 15f;
+
+        #endregion
     }
 }
