95f2bcbdc7e0f10464a6c59d3aad32d1349e22fe
LaM0uette <LaM0uette.dev@gmail.com>
2024-02-15 18:22:49 +0100

    clean

--- Assets/Game/Modules/Ads/RewardedAdsGem.cs
+++ Assets/Game/Modules/Ads/RewardedAdsGem.cs
@@ -1,4 +1,3 @@
-using System;
 using Game.Modules.Utils;
 using Obvious.Soap;
 using UnityEngine;
@@ -14,11 +13,10 @@ namespace Game.Modules.Ads
         [SerializeField] private string _androidAdUnitId = "Rewarded_Android";
         [SerializeField] private string _iOSAdUnitId = "Rewarded_iOS";
         
-        private string _adUnitId = null; // This will remain null for unsupported platforms
+        private string _adUnitId;
 
         private void Awake()
         {
-            // Get the Ad Unit ID for the current platform:
 #if UNITY_IOS
         _adUnitId = _iOSAdUnitId;
 #elif UNITY_ANDROID
@@ -35,28 +33,22 @@ namespace Game.Modules.Ads
 
         #region Functions
 
-        // Call this public method when you want to get an ad ready to show.
         public void LoadAd()
         {
-            // IMPORTANT! Only load content AFTER initialization (in this example, initialization is handled in a different script).
             Debug.Log("Loading Ad: " + _adUnitId);
             Advertisement.Load(_adUnitId, this);
         }
 
-        // If the ad successfully loads
         public void OnUnityAdsAdLoaded(string adUnitId)
         {
             Debug.Log("Ad Loaded: " + adUnitId);
         }
 
-        // Implement a method to execute when the user clicks the button:
         public void ShowAd()
         {
-            // Show the ad:
             Advertisement.Show(_adUnitId, this);
         }
 
-        // Implement the Show Listener's OnUnityAdsShowComplete callback method to determine if the user gets a reward:
         public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState)
         {
             if (adUnitId.Equals(_adUnitId) && showCompletionState.Equals(UnityAdsShowCompletionState.COMPLETED))
@@ -74,20 +66,15 @@ namespace Game.Modules.Ads
             }
         }
 
-        // Implement Load and Show Listener error callbacks:
         public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
         {
             Debug.Log($"Error loading Ad Unit {adUnitId}: {error.ToString()} - {message}");
-            // Use the error details to determine whether to try to load another ad.
-            
             LoadAd();
         }
 
         public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
         {
             Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
-            // Use the error details to determine whether to try to load another ad.
-            
             LoadAd();
         }
 
