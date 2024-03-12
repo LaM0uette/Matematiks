692f0e3b3d14b4373fb73ab007a7e08ce45b5f84
LaM0uette <LaM0uette.dev@gmail.com>
2024-03-12 20:26:43 +0100

    middle score handler

--- Assets/Game/Ui/Menu/MenuHandler.cs
+++ Assets/Game/Ui/Menu/MenuHandler.cs
@@ -1,3 +1,4 @@
+using Game.Modules.Utils;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,11 +7,17 @@ namespace Game.Ui.Menu
     public class MenuHandler : MonoBehaviour
     {
         #region Statements
+        
+        private const string _midleScoreKey = "MiddleScoreValue";
+        private const string _midleScoreTitleKey = "MiddleScoreTitle";
 
         private UIDocument _uiDocument;
         private VisualElement _root;
         
         private HeaderScores.HeaderScores _headerScores;
+        
+        private Label _midleScoreLabel;
+        private Label _midleScoreTitleLabel;
         
         private void Awake()
         {
@@ -18,11 +25,13 @@ namespace Game.Ui.Menu
             _root = _uiDocument.rootVisualElement;
 
             SetupHeaderScores();
+            SetupMiddleScore();
         }
 
         private void Start()
         {
             InitHeaderScores();
+            InitMiddleScore();
         }
 
         #endregion
@@ -33,10 +42,23 @@ namespace Game.Ui.Menu
         {
             _headerScores = _root.Q<HeaderScores.HeaderScores>();
         }
-        
         private void InitHeaderScores()
         {
             _headerScores.UpdateScore();
+        }
+        
+        private void SetupMiddleScore()
+        {
+            _midleScoreLabel = _root.Q<Label>(_midleScoreKey);
+            _midleScoreTitleLabel = _root.Q<Label>(_midleScoreTitleKey);
+        }
+        private void InitMiddleScore()
+        {
+            var currentScore = Saver.CurrentScore.LoadInt();
+            _midleScoreTitleLabel.text = currentScore > 0 ? "CURRENT SCORE" : "LAST SCORE";
+            
+            var score = currentScore > 0 ? currentScore : Saver.LastScore.LoadInt();
+            _midleScoreLabel.text = score.ToString();
         }
 
         #endregion
